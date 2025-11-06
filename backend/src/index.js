2d8498cb4e73fb06250971bcfac464335489ee97
codeRishal <skullrishal@gmail.com>
2025-11-06 19:13:59 +0530

    Implement user authentication with signup, login, and logout functionality; add user model and database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,23 @@
 import express from "express"
+import dotenv from"dotenv"
+
+import {connectDB} from "./lib/db.js"
 import authRoutes from"./routes/auth.routes.js"
 
+dotenv.config()
+
+
 const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+const PORT=process.env.PORT;
+console.log("PORT from env:", process.env.PORT);
+
 app.use("/api/auth", authRoutes)
-app.listen(5001, ()=>{
-    console.log("sever is running on port 5001 ");
-    
+
+app.listen(PORT, ()=>{
+    console.log("sever is running on port:"+PORT);
+    connectDB();
 })
